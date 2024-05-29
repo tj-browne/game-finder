9d84755d2804add325b2d696a57c7bfa2d7973ec
TJ <t.browne122@gmail.com>
2024-05-29 16:55:44 +0100

    Add Basic HTML & CSS

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
+
+
+def details(request):
+    return render(request, 'main/details.html')
