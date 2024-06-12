280201fb2d51a4ec486afb03cf57ef196fb45b3e
TJ <t.browne122@gmail.com>
2024-06-12 18:11:48 +0100

    Add Basic API Access Validation

--- main/forms.py
+++ main/forms.py
@@ -2,5 +2,16 @@ from django import forms
 
 
 class CredentialsForm(forms.Form):
-    client_id = forms.CharField(label='Client ID', max_length=255, min_length=1)
-    access_token = forms.CharField(label='Access Token', widget=forms.PasswordInput, max_length=255, min_length=1)
+    client_id = forms.CharField(label='Client ID', max_length=30, min_length=30, required=True)
+    access_token = forms.CharField(label='Access Token', widget=forms.PasswordInput, max_length=30, min_length=30,
+                                   required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        client_id = cleaned_data.get("client_id")
+        access_token = cleaned_data.get("access_token")
+
+        if not client_id or not access_token:
+            raise forms.ValidationError("Client ID and Access Token are required.")
+
+        return cleaned_data
