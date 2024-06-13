4fd8d1d75ad11d14184218ee7ba56bf31a552fff
TJ <t.browne122@gmail.com>
2024-06-13 13:48:19 +0100

    Add Client Secret Auth

--- main/api_service.py
+++ main/api_service.py
@@ -7,13 +7,30 @@ def get_covers(game, size='t_cover_big'):
 
 
 class apiService:
-    # TODO: Add client secret to API Authentication
     def __init__(self, request):
         self.client_id = request.session.get('client_id')
         self.access_token = request.session.get('access_token')
-        if not self.client_id or not self.access_token:
-            raise ValueError("Invalid Client ID or Access Token.")
+        self.client_secret = request.session.get('client_secret')
+        if not self.access_token:
+            self.authenticate()
+            request.session['access_token'] = self.access_token
         self.base_url = 'https://api.igdb.com/v4'
+
+    def authenticate(self):
+        auth_url = 'https://id.twitch.tv/oauth2/token'
+        data = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials'
+        }
+        response = requests.post(auth_url, data=data)
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+        response_data = response.json()
+
+        if 'access_token' in response_data:
+            self.access_token = response_data['access_token']
+        else:
+            raise ValueError("Failed to authenticate. Check client credentials.")
 
     def get_headers(self):
         return {
--- main/forms.py
+++ main/forms.py
@@ -3,15 +3,20 @@ from django import forms
 
 class CredentialsForm(forms.Form):
     client_id = forms.CharField(label='Client ID', max_length=30, min_length=30, required=True)
-    access_token = forms.CharField(label='Access Token', widget=forms.PasswordInput, max_length=30, min_length=30,
-                                   required=True)
+    # access_token = forms.CharField(label='Access Token', widget=forms.PasswordInput, max_length=30, min_length=30)
+    client_secret = forms.CharField(label='Client Secret', widget=forms.PasswordInput, max_length=30, min_length=30, required=True)
 
     def clean(self):
         cleaned_data = super().clean()
         client_id = cleaned_data.get("client_id")
-        access_token = cleaned_data.get("access_token")
+        # access_token = cleaned_data.get("access_token")
+        client_secret = cleaned_data.get("client_secret")
 
-        if not client_id or not access_token:
-            raise forms.ValidationError("Client ID and Access Token are required.")
+        if not client_id:
+            raise forms.ValidationError("Client ID is invalid.")
+        # if not access_token:
+        #     raise forms.ValidationError("Access Token is invalid.")
+        if not client_secret:
+            raise forms.ValidationError("Client Secret is invalid.")
 
         return cleaned_data
